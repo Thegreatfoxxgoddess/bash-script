b656eb49b4862f2dfa17a2e9b1428b20e0f3827f
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-11-05 19:25:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@
 #######################################
 
 echo "Welcome Alicia"
-echo "sit back and enjoy"
+echo "sit back and enjoy while I do my work ;)"
 ## Update packages and Upgrade system
 sudo apt-get update -y
 
 ## Git ##
-echo '###Installing Git..'
+echo 'Installing Git..'
 sudo apt-get install git -y
+
+##Additional Repos##
+
+echo 'updating repos.....'
